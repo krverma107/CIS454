133f5730f19212b70c94acfcf4b1009d7e63c436
maxkaufman <91966735+maxkaufman@users.noreply.github.com>
2024-03-05 14:25:57 -0500

    Update and rename Placeholder.txt to User.java

---
+++ Classes/User.java
@@ -0,0 +1,50 @@
+public class User {
+    private String name;
+    private String password;
+    private String username;
+    private String email;
+
+    // Constructor
+    public User(String name, String password, String username, String email) {
+        this.name = name;
+        this.password = password;
+        this.username = username;
+        this.email = email;
+    }
+
+    //Name methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Password methods
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //Username methods
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    //Email methods
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
