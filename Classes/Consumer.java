4b337dee801ecc336f423aca1b2ef42f71e5a87c
maxkaufman <91966735+maxkaufman@users.noreply.github.com>
2024-03-07 14:01:45 -0500

    Update Consumer.java

--- Classes/Consumer.java
+++ Classes/Consumer.java
@@ -1,1 +1,21 @@
+import java.util.ArrayList;
+
+public class Consumer extends User {
+    private ArrayList<Object> consumerHistory;
+
+    public Consumer(String name, String password, String username, String email) {
+        super(name, password, username, email);
+        this.consumerHistory = new ArrayList<>();
+    }
+
+    //returns consumer history
+    public ArrayList<Object> getConsumerHistory() {
+        return consumerHistory;
+    }
+
+    //appends new order object to consumer history (presumably order but can be changed if need be)
+    public void addConsumerHistory(Object order) {
+        consumerHistory.add(order);
+    }
+}
 
