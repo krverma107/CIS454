009edebbf28c2e2a85883fbd58ade03010eebed2
jjness18 <91549858+jjness18@users.noreply.github.com>
2024-03-07 23:17:50 -0500

    Delete Admin.java

--- Admin.java
+++
@@ -1,7 +0,0 @@
-public class Admin extends User {
-    private int pantryid;
-
-    public Admin(String name, String password, String username, String email, int pantryid) {
-        super(name, password, username, email, pantryid);
-
-}
