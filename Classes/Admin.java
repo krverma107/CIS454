c64766eae4f064f4ad10454cd3898ea156689725
jjness18 <91549858+jjness18@users.noreply.github.com>
2024-03-07 23:17:17 -0500

    Add files via upload

---
+++ Classes/Admin.java
@@ -0,0 +1,7 @@
+public class Admin extends User {
+    private int pantryid;
+
+    public Admin(String name, String password, String username, String email, int pantryid) {
+        super(name, password, username, email, pantryid);
+
+}
