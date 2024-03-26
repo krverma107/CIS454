6928a783384a6594f44535c3c75ceda6d0253ba6
Jack Gorman <91551281+JackGorman3@users.noreply.github.com>
2024-03-26 16:16:49 -0400

    Update Stock.java

--- Classes/Stock.java
+++ Classes/Stock.java
@@ -1,4 +1,60 @@
+// Node.java
+public class Node {
+    public Stock data;
+    public Node next;
 
+    // Constructor
+    public Node(Stock data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+// LinkedList.java
+public class LinkedList {
+    private Node head;
+
+    // Constructor
+    public LinkedList() {
+        this.head = null;
+    }
+
+    // Method to insert a stock at the beginning of the list
+    public void insert(Stock stock) {
+        Node newNode = new Node(stock);
+        newNode.next = head;
+        head = newNode;
+    }
+
+}
+
+
+// Stock.java
 public class Stock {
+    private String name;
+    private int quantity;
 
+    // Constructor
+    public Stock(String name, double quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity += quantity;
+    }
 }
