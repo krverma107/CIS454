a259562442bf0402e18a6f39df4b22e2b34df3fd
Jack Gorman <91551281+JackGorman3@users.noreply.github.com>
2024-03-26 16:27:46 -0400

    Update Stock.java

--- Classes/Stock.java
+++ Classes/Stock.java
@@ -1,3 +1,33 @@
+// Stock.java
+public class Stock {
+    private String name;
+    private int quantity;
+
+    // Constructor
+    public Stock(String name, double quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+}
+
+
 // Node.java
 public class Node {
     public Stock data;
@@ -36,34 +66,4 @@ public class LinkedList {
         }
     }
 
-}
-
-
-// Stock.java
-public class Stock {
-    private String name;
-    private int quantity;
-
-    // Constructor
-    public Stock(String name, double quantity) {
-        this.name = name;
-        this.quantity = quantity;
-    }
-
-    // Getters and setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity += quantity;
-    }
 }
