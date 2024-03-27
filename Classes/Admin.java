11c5ef57f01fd235c352c6cdc75964d9bb1d3a0d
jjness18 <91549858+jjness18@users.noreply.github.com>
2024-03-26 21:49:38 -0400

    Update Admin.java

--- Classes/Admin.java
+++ Classes/Admin.java
@@ -6,6 +6,7 @@ public class Admin extends User {
         this.pickUpTimes = new ArrayList<>();
     }
 
+    // Add pick up times in military time (e.g. 8:00, 12:00, 16:00)
     public List<String> addPickUpTimes(String time) {
         pickupTimes.add(time);
     }
